87147f809f6556fea299101bb0e79ad7f154a29d
iAmSomeBoy <ashikur2401@gmail.com>
2025-11-16 10:52:04 +0600

    .....

---
+++ Var_syntax.java
@@ -0,0 +1,9 @@
+public class Var_syntax {
+    public static void main(){
+        var x= 5;
+        var s= "Hello World";
+
+        System.out.println(x);
+        System.out.println(s);
+    }
+}
