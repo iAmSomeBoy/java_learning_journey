5837fb2fbb7de7a971028a0349aed6f911a0aad4
iAmSomeBoy <ashikur2401@gmail.com>
2025-11-28 21:43:18 +0600

    added

--- my_space/java_swing/MyDrawPanel.java
+++ my_space/java_swing/MyDrawPanel.java
@@ -3,18 +3,19 @@ import java.awt.*;
 
 class MyDrawPanel extends JPanel {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Draw Panel");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(new MyDrawPanel());
-            frame.setSize(300, 300);
-            frame.setVisible(true);
-        });
+
+        JFrame frame = new JFrame("Draw Panel");
+        JButton button= new JButton();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MyDrawPanel());        
+        frame.setSize(300, 600);
+        frame.setVisible(true);
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.orange);
-        g.fillRect(20, 50, 100, 100);
+        g.fillRect(20, 30, 100, 200);
     }
 }
