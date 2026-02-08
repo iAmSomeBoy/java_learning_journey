df2dde0d4de683c523e0ca58dce9858e7b537e1d
iAmSomeBoy <ashikur2401@gmail.com>
2026-02-08 12:19:08 +0600

    Refactor code structure for improved readability and maintainability

--- academics/trash_codes/drafts/Demo.java
+++ academics/trash_codes/drafts/Demo.java
@@ -1,6 +1,5 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello from Demo class!");
-        // Add your code here
-    }
+package academics;
+
+public class demo {
+    
 }
